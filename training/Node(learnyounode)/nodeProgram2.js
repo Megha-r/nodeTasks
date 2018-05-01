c08beab066044983b8ad717a00783b37b4721ce9
Megha <megha.r@successivesoftwares.com>
2018-05-01 18:03:11 +0530

    node tasks 1,2,3

---
+++ training/Node(learnyounode)/nodeProgram2.js
@@ -0,0 +1,34 @@
+/********************************* Task 4 ***********************************************
+ *   Write a program that uses a single asynchronous filesystem operation to  
+     read a file and print the number of newlines it contains to the console 
+ */
+
+ const fs= require('file-system')
+ fs.writeFile('file/test.txt','aa \n helloo \n ', function(err){})
+
+ fs = require('fs')
+ var newLines =  undefined;
+ function addOne(callback) {
+    fs.readFile('file/test.txt', 'utf8', function (err,data) {
+        if (err) {
+          return console.log("There is an error");
+        }
+        else{
+         
+           newLines = data.toString().split('\n').length - 1
+           console.log("inside else")
+          callback()
+
+        }
+       
+
+       
+      });
+    
+  }
+  
+  function myFunc() {
+    console.log(newLines);
+}
+  
+  addOne(myFunc);
