61df7c05b160299e0516dddf46fb7220bd32a2ac
Megha <megha.r@successivesoftwares.com>
2018-05-02 10:27:56 +0530

    updated Task 5

--- training/Node_learnyounode/nodeProgram3.js
+++ training/Node_learnyounode/nodeProgram3.js
@@ -1,28 +1,15 @@
 
-// var fs = require('fs')
-// var path = require ('path')
-// const testFolder = './desktop/';
-// fs.readdir(testFolder, function(err, list) {
-//     list.forEach(function(file){
-//         if (path.extname(file) === '.' + testFolder)
-//         console.log(file);
-    
-//     })
-//   })
-
-var fs = require('fs');
-var path = require('path');
-var mydir = process.argv[2];
-var ext1 = process.argv[3]
-fs.readdir(mydir, function(err, files){
-  if(err){
-    throw err
-  }
-  //console.log(files);
-  files.forEach(function(filename){
-    var ext = path.extname(filename);
-    if(ext == ext1){
-      console.log(filename);
-    }
-  });
-});
+/********************************************  Task 5 ********************************************
+Create a program that prints a list of files in a given directory, filtered by the extension of the files. 
+ ************************************************************************************** */
+  var fs = require('fs')
+  var path = require('path')
+  
+  fs.readdir(process.argv[2], function (err, list) 
+  {
+    list.forEach(function (file)
+     {
+      if (path.extname(file) === '.' + process.argv[3])
+        console.log(file)
+    })
+  })
