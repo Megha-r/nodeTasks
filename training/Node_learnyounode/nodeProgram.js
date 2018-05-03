8537457411aaec86310ca9114cbcd83141ae176b
Megha <megha.r@successivesoftwares.com>
2018-05-03 09:35:42 +0530

    Task 1,2,3 verified

--- training/Node_learnyounode/nodeProgram.js
+++ training/Node_learnyounode/nodeProgram.js
@@ -1,45 +1,45 @@
 //***************************************  Task 1 ********************************* */
 //***************** Print hello world in terminal ********************************* */
 
-console.log("Hello World");
+//console.log("HELLO WORLD")
 
 
 /***************************************  Task 2 ********************************* 
   Write a program that accepts one or more numbers as command-line arguments  
   and prints the sum of those numbers to the console (stdout). */
 
-const arr = process.argv.length;
-var sum;
-for (var i = 2; i < arr; i++) {
+// var result= 0;
+// for (var i = 2; i < process.argv.length; i++) {
     
-    sum += process.argv[i]
-  }
-console.log("sum is-",sum);
-
-// console.log("Sum is",+process.argv[2]);
+//     result += Number(process.argv[i])
+//   }
+//  console.log( result);
 
 
 /***************************************  Task 3 ********************************* 
            Write a program that uses a single synchronous filesystem operation to  
           read a file and print the number of newlines (\n) it contains to the */ 
 
-const fs = require('file-system')
+const fs = require('fs') 
 //console.log(fs)
+var contents = fs.readFileSync(process.argv[2])
+var lines = contents.toString().split('\n').length - 1
+console.log(lines);
 
-//fs.writeFile('file/test.txt','aa', {})
-fs.readFileSync('file/test.txt', 'utf8', function (err,data) {
+// //fs.writeFile('file/test.txt','aa', {})
+// fs.readFileSync('file/test.txt', 'utf8', function (err,data) {
  
-  if (err) {
-    return console.log("There is an error");
-  }
+//   if (err) {
+//     return console.log("There is an error");
+//   }
   
-  else{
-   
-    var newLines = data.toString().split('\n').length - 1
-    console.log(newLines);
-  }
+  
+  
+//   var newLines = data.toString().split('\n').length - 1
+
+  
 
  
-});
+
 
 
